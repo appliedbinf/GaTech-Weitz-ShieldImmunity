0e19dad85255b101ebae9b82d1bfff9ff70e3107
Aroon Chande <git@atc.io>
2021-01-24 15:54:10 -0500

    Update install_libraries.R to match new reqs closes #1

--- install_libraries.R
+++ install_libraries.R
@@ -1,11 +1,7 @@
-packages = c("shiny", "shinydashboard", "shinydashboardPlus", 
-"dashboardthemes", "shinyjqui", "shinyjs", 
-"shinypop", "ggplot2", "plotly", "DBI", "RSQLite", 
-"future.callr", "promises", "tibble", "plyr", 
-"stringr", "data.table", "tidyr", "dplyr", 
-"textshape", "DT", "forcats", "jsonlite", 
-"reticulate", "shinyWidgets", "waiter", "rintrojs",
-"cowplot", "deSolve", "reshape2", "remotes")
+packages = c("shiny", "tidyverse", 
+"shinyWidgets", "waiter", "cowplot", 
+"deSolve", "reshape2", "remotes"
+)
 
 install.packages(packages)
 
