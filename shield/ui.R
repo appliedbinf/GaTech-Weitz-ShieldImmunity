75c91061113b3b5f153340866a2ea47966cb9e86
Aroon Chande <git@atc.io>
2021-01-24 12:32:56 -0500

    Shield model: notation (greek letters) #9

--- shield/ui.R
+++ shield/ui.R
@@ -1,19 +1,24 @@
 source('../libraries.R')
 library(waiter)
 
-ui <- sidebarLayout(sidebarPanel(
+ui <- sidebarLayout(
+    sidebarPanel(
     noUiSliderInput(
         inputId = "alpha",
-	label="Alpha (strength of shielding)",
+	label=HTML("Strength of shielding (&alpha;)"),
         min = 0,
         max = 20,
         value = 2
     ),
-    awesomeRadio(
+	radioButtons(
         "severity",
-        "Low or High R0 outbreak",
-        choices = c("Low R0" = "low", "High R0" = "high"),
-        selected = "low",
+        HTML(paste0("Low or High R", tags$sub("0"), " outbreak")),
+        # "Low or High R",
+        # choices = c(HTML(paste0("Low R", tags$sub("0"))), paste0("High R", tags$sub("0"))),
+        # choices = c("Low R" = "low", "High R" = "high"),
+        choiceValues = c("low", "high"), 
+        choiceNames =  list(span(HTML("Low R<sub>0</sub>")), span(HTML("High R<sub>0</sub>"))),
+        # selected = "low",
         inline = T
     )
 ),
@@ -21,7 +26,7 @@ ui <- sidebarLayout(sidebarPanel(
 mainPanel(fluidPage(
     use_waiter(),
     tags$script(src = "iframeResizer.contentWindow.min.js"),
-    fluidRow(echarts4rOutput("p_Dday")),
-    fluidRow(echarts4rOutput("p_Hacu_day")),
+    fluidRow(echarts4rOutput("p_Dday"), br(), hr()),
+    fluidRow(echarts4rOutput("p_Hacu_day"), br(), hr()),
     fluidRow(echarts4rOutput("p_D_byage"))
 )))
